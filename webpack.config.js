2114edc60ff2630180aef1ff3a83824556d24ac0
Rafael Saback <rafaelsaback@gmail.com>
2018-10-29 17:59:24 +0100

    Cosmetic changes

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,33 @@
 path = require("path");
 
 module.exports = {
-  mode: 'development',
-  entry: ['./src/scripts/Controller.ts', './src/styles/main.scss'],
-  output: {
-    filename: 'bundle.js',
-    path: path.join(__dirname, 'dist')
-  },
-  devtool: "source-map",
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        loader: 'ts-loader',
-        exclude: /node_modules/,
-      },
-      {
-        test:/\.(s*)css$/,
-        use:['style-loader','css-loader', 'sass-loader']
-      }
-    ]
-  },
-  resolve: {
-    extensions: [".tsx", ".ts", ".js"]
-  },
+    mode: 'production',
+    entry: ['./src/scripts/Controller.ts', './src/styles/main.scss'],
+    output: {
+        filename: 'bundle.js',
+        path: path.join(__dirname, 'dist')
+    },
+    devtool: "source-map",
     devServer: {
         inline: true,
         contentBase: path.join(__dirname, 'dist'),
         port: 3000,
         open: true
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                loader: 'ts-loader',
+                exclude: /node_modules/,
+            },
+            {
+                test:/\.(s*)css$/,
+                use:['style-loader','css-loader', 'sass-loader']
+            }
+        ]
+    },
+    resolve: {
+        extensions: [".tsx", ".ts", ".js"]
     },
 };
