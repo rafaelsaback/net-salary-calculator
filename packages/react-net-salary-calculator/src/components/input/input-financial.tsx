0fa52dac13b245322ea088d185c4e0db1acbfab0
Rafael Saback <rafaelsaback@gmail.com>
2019-08-29 00:09:48 +0200

    Connect salary input with Redux store

--- packages/react-net-salary-calculator/src/components/input/input-financial.tsx
+++ packages/react-net-salary-calculator/src/components/input/input-financial.tsx
@@ -1,17 +1,16 @@
-import React, { FunctionComponent } from 'react';
+import React, { FunctionComponent, ChangeEvent } from 'react';
 import {
   TextField,
   makeStyles,
   FormControl,
   FormLabel,
 } from '@material-ui/core';
-import { handleChange } from '../../helpers/utils';
 import { BOLD_BLACK } from '../../helpers/consts';
 
 interface InputSalaryProps {
   label: string;
   value: string;
-  setValue(value: string): void;
+  setValue(event: ChangeEvent<HTMLInputElement>): void;
   required: boolean;
 }
 
@@ -41,13 +40,11 @@ const InputFinancial: FunctionComponent<InputSalaryProps> = ({
     >
       <FormLabel component="label">{label}</FormLabel>
       <TextField
-        // label={label}
         value={value}
         placeholder={'0.00 PLN'}
-        onChange={handleChange(setValue)}
+        onChange={setValue}
         margin="normal"
         fullWidth
-        // variant="outlined"
       />
     </FormControl>
   );
--- packages/react-net-salary-calculator/src/components/input/uop-form.tsx
+++ packages/react-net-salary-calculator/src/components/input/uop-form.tsx
@@ -1,28 +1,28 @@
 import React, { FunctionComponent } from 'react';
 import { Container } from '@material-ui/core';
+
 import InputPeriod from './input-period';
 import InputFinancial from './input-financial';
 import { LABELS } from '../../helpers/consts';
+import { useSelector, useDispatch } from 'react-redux';
+import { selectUOPParam } from '../../helpers/selectors';
+import { setSalary } from '../../helpers/utils';
 
 interface UOPFormProps {
-  salary: string;
   period: string;
-  setSalary(value: string): void;
   setPeriod(value: string): void;
 }
 
-const UOPForm: FunctionComponent<UOPFormProps> = ({
-  salary,
-  period,
-  setSalary,
-  setPeriod,
-}) => {
+const UOPForm: FunctionComponent<UOPFormProps> = ({ period, setPeriod }) => {
+  const salary = useSelector(selectUOPParam('salary')) || '';
+  const dispatch = useDispatch();
+
   return (
     <Container>
       <InputFinancial
         label={LABELS.UOP.SALARY}
-        value={salary}
-        setValue={setSalary}
+        value={salary.toString()}
+        setValue={setSalary(dispatch)}
         required={true}
       />
       <InputPeriod period={period} setPeriod={setPeriod} />
