e61b55362b513bb5ddb89a7376060d8634337478
Rafael Saback <rafaelsaback@gmail.com>
2019-08-29 00:07:29 +0200

    Set up Redux devtools

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,6 +1,12 @@
 import { createStore } from 'redux';
 import reducer from './reducer';
+import { initialState } from '../helpers/consts';
 
-const store = createStore(reducer);
+const store = createStore(
+  reducer,
+  initialState,
+  (window as any).__REDUX_DEVTOOLS_EXTENSION__ &&
+    (window as any).__REDUX_DEVTOOLS_EXTENSION__(),
+);
 
 export default store;
