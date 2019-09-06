a31c159c7fd53547eccd8b0f276a29b7106bd472
Rafael Saback <rafaelsaback@gmail.com>
2019-09-06 20:53:51 +0200

    Remove side padding for the main container

--- src/app.tsx
+++ src/app.tsx
@@ -4,12 +4,20 @@ import Results from './components/results';
 import { useSelector } from 'react-redux';
 import { selectShowResults } from './helpers/selectors';
 import Container from '@material-ui/core/Container';
+import makeStyles from '@material-ui/styles/makeStyles';
+
+const useStyles = makeStyles({
+  root: {
+    padding: '16px 0',
+  },
+});
 
 const App: FunctionComponent = () => {
   const showResults = useSelector(selectShowResults);
+  const classes = useStyles({});
 
   return (
-    <Container>
+    <Container className={classes.root}>
       <InputForm />
       {showResults && <Results />}
     </Container>
