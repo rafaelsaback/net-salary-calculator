8b76a0d281a3949bd35b3fef333db844c9ee602e
Rafael Saback <rafaelsaback@gmail.com>
2020-02-09 15:30:29 +0100

    Update PIT/ZUS values for 2020

--- packages/react-net-salary-calculator/src/helpers/consts.ts
+++ packages/react-net-salary-calculator/src/helpers/consts.ts
@@ -15,7 +15,7 @@ export const BOX_SHADOW = {
     '0px 1px 3px 0px rgba(0,0,0,0.2), 0px 1px 1px 0px rgba(0,0,0,0.14), 0px 2px 1px -1px rgba(0,0,0,0.12)',
 };
 
-export const BORDER_RADIUS = { borderRadius: '0.5rem' };
+export const BORDER_RADIUS = { borderRadius: '0.5rem', overflow: 'hidden' };
 
 export const LABELS = {
   UOP: {
@@ -100,15 +100,48 @@ export const HEALTH_CONTRIBUTION_RATE = 9 / 100;
 export const HEALTH_DEDUCTIBLE_RATE = 7.75 / 100;
 export const MONTHLY_RELIEF = 46.33;
 
-// UOP
-export const ANNUAL_LIMIT = 133290; // Annual limit for pension and disability calculations
-export const EARNING_COST = 111.25; // Koszt uzyskania przychodu
+// =====================================================
+//                        UOP
+// =====================================================
+
+// Annual limit for pension and disability calculations
+// Source: https://www.pit.pl/ograniczenia-skladek-zus/
+export const ANNUAL_LIMIT = 156810;
+
+// Koszt uzyskania przychodu
+// Source: https://www.pit.pl/koszty-uzyskania-przychodu-pit/
+export const EARNING_COST = 250;
+
 export const DISABILITY_RATE = 1.5 / 100; // rentowe
 export const PENSION_RATE = 9.76 / 100; // emerytalne
 export const SICKNESS_RATE = 2.45 / 100; // chorobowe
 
-export const RATE_18 = 0.18;
+export const RATE_17 = 0.17;
 export const RATE_19 = 0.19;
 export const RATE_32 = 0.32;
 
-export const MINIMUM_WAGE = 2250;
+// Source: https://en.wikipedia.org/wiki/Minimum_wage_in_Poland
+export const MINIMUM_WAGE = 2600;
+
+// =====================================================
+//                        B2B
+// =====================================================
+
+// Source: https://www.zus.pl/baza-wiedzy/skladki-wskazniki-odsetki/skladki/wysokosc-skladek-na-ubezpieczenia-spoleczne
+
+export const HEALTH_INSURANCE = 362.34;
+
+export const SMALL_ZUS = {
+  PENSION: 152.26, // emerytalne
+  DISABILITY: 62.4, // rentowe
+  SICKNESS: 19.11, //chorobowe
+  ACCIDENT: 13.02, // wypadkowe
+};
+
+export const NORMAL_ZUS = {
+  PENSION: 612.19, // emerytalne
+  DISABILITY: 250.9, // rentowe
+  SICKNESS: 76.84, // chorobowe
+  ACCIDENT: 52.37, // wypadkowe
+  LABOR_FUND: 76.84, // fundusz pracy
+};
