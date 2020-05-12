d009a90d9508ce640ef295a802029df654189bd6
Rafael Saback <rafaelsaback@gmail.com>
2020-05-12 23:20:41 +0200

    Create basic tab style

--- packages/mobile/src/app-entry.tsx
+++ packages/mobile/src/app-entry.tsx
@@ -4,20 +4,41 @@ import { Container, Content, Tab, Tabs } from 'native-base';
 
 const styles = StyleSheet.create({
   mainContainer: { paddingTop: 25 },
+  tabStyle: { backgroundColor: '#fff' },
+  tabTextStyle: { color: 'rgba(0, 0, 0, 0.54)' },
+  activeTabTextStyle: { color: '#1976d2' },
+  tabUnderlineStyle: { backgroundColor: '#1976d2' },
 });
 
 const AppEntry: React.FC = () => {
   return (
     <Container style={styles.mainContainer}>
-      <Tabs scrollWithoutAnimation tabBarPosition={'bottom'}>
-        <Tab heading="Umowa o Pracę">
+      <Tabs
+        scrollWithoutAnimation
+        locked
+        tabBarPosition={'bottom'}
+        tabBarUnderlineStyle={styles.tabUnderlineStyle}
+      >
+        <Tab
+          tabStyle={styles.tabStyle}
+          activeTabStyle={styles.tabStyle}
+          textStyle={styles.tabTextStyle}
+          activeTextStyle={styles.activeTabTextStyle}
+          heading="Umowa o Pracę"
+        >
           <Content>
             <Text>Umowa o pracę</Text>
           </Content>
         </Tab>
-        <Tab heading="B2B Contract">
+        <Tab
+          tabStyle={styles.tabStyle}
+          activeTabStyle={styles.tabStyle}
+          textStyle={styles.tabTextStyle}
+          activeTextStyle={styles.activeTabTextStyle}
+          heading="B2B Contract"
+        >
           <Content>
-            <Text>B2B AAAA Contract</Text>
+            <Text>B2B Contract</Text>
           </Content>
         </Tab>
       </Tabs>
