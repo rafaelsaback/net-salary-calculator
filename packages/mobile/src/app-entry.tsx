2e32467db82735716b999669d439b7d1d6aac170
Rafael Saback <rafaelsaback@gmail.com>
2020-05-12 22:25:24 +0200

    Create own entry file

---
+++ packages/mobile/src/app-entry.tsx
@@ -0,0 +1,13 @@
+import { Text, View } from 'react-native';
+import React from 'react';
+import styles from './app-entry.style';
+
+const AppEntry: React.FC = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.tsx to start working on your app!</Text>
+    </View>
+  );
+};
+
+export default AppEntry;
